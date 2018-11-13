059f3275130583c1ffc28a1bb6b2522fe2434b1e
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-13 16:33:10 +0800

    获取单个客户端Banner

---
+++ lichkin-projects-app-apis-common/src/main/java/com/lichkin/application/apis/GetBanner/O.java
@@ -0,0 +1,57 @@
+package com.lichkin.application.apis.GetBanner;
+
+import com.lichkin.framework.defines.enums.impl.LKClientTypeEnum;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class O {
+
+	/** 主键 */
+	private String id;
+
+	/** 客户端唯一标识 */
+	private String appKey;
+
+	/** 客户端类型（枚举） */
+	private LKClientTypeEnum clientType;
+
+	/** 版本号（分号拼接x.y.z） */
+	private String versions;
+
+	/** 分类编码（字典） */
+	private String categoryCode;
+
+	/** 模板编码（字典） */
+	private String templateCode;
+
+	/** 标题（超出...） */
+	private String title;
+
+	/** 简介（超出...） */
+	private String brief;
+
+	/** 关键字（逗号拼接） */
+	private String keywords;
+
+	/** 作者姓名 */
+	private String author;
+
+	/** 内容（body内直接可以用于展现的内容） */
+	private String content;
+
+	/** 外链地址 */
+	private String linkUrl;
+
+	/** 排序号 */
+	private Byte orderId;
+
+	/** 发布时间（yyyyMMddHHmmssSSS） */
+	private String publishTime;
+
+	/** 图片（URL） */
+	private String banner;
+
+}
