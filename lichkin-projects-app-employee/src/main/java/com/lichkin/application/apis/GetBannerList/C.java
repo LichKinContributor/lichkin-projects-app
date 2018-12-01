3c1980bd91dff91b0dd0a2fd538a2ca68ebafbfd
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-12-01 16:37:51 +0800

    员工版 获取新闻、获取Banner 登录前不响应数据

--- lichkin-projects-app-employee/src/main/java/com/lichkin/application/apis/GetBannerList/C.java
+++ lichkin-projects-app-employee/src/main/java/com/lichkin/application/apis/GetBannerList/C.java
@@ -2,22 +2,42 @@ package com.lichkin.application.apis.GetBannerList;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lichkin.framework.defines.LKFrameworkStatics;
+import com.lichkin.framework.defines.enums.LKCodeEnum;
 import com.lichkin.framework.defines.exceptions.LKException;
+import com.lichkin.framework.defines.exceptions.LKRuntimeException;
 import com.lichkin.framework.web.annotations.LKApiType;
 import com.lichkin.framework.web.enums.ApiType;
 import com.lichkin.springframework.controllers.ApiKeyValues;
 import com.lichkin.springframework.controllers.LKApiYYController;
 import com.lichkin.springframework.services.LKApiService;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 @RestController(Statics.CONTROLLER_NAME)
 @RequestMapping(value = LKFrameworkStatics.WEB_MAPPING_API + Statics.SUB_URL)
 @LKApiType(apiType = ApiType.COMPANY_QUERY)
 public class C extends LKApiYYController<I, List<O>, List<O>> {
+
+	@Getter
+	@RequiredArgsConstructor
+	enum ErrorCodes implements LKCodeEnum {
+
+		/** 不是员工 */
+		YOU_ARE_NOT_A_EMPLOYEE(29003),
+
+		;
+
+		private final Integer code;
+
+	}
+
 
 	@Override
 	protected boolean saveLog(I cin, ApiKeyValues<I> params) {
@@ -32,6 +52,14 @@ public class C extends LKApiYYController<I, List<O>, List<O>> {
 	@Override
 	protected LKApiService<I, List<O>> getService(I cin, ApiKeyValues<I> params) {
 		return service;
+	}
+
+
+	@Override
+	protected void beforeInvokeService(I cin, ApiKeyValues<I> params) throws LKException {
+		if (StringUtils.isBlank(cin.getDatas().getToken())) {
+			throw new LKRuntimeException(ErrorCodes.YOU_ARE_NOT_A_EMPLOYEE);
+		}
 	}
 
 
--- lichkin-projects-app-employee/src/main/java/com/lichkin/application/apis/GetNewsPage/C.java
+++ lichkin-projects-app-employee/src/main/java/com/lichkin/application/apis/GetNewsPage/C.java
@@ -1,22 +1,42 @@
 package com.lichkin.application.apis.GetNewsPage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lichkin.framework.defines.LKFrameworkStatics;
+import com.lichkin.framework.defines.enums.LKCodeEnum;
 import com.lichkin.framework.defines.exceptions.LKException;
+import com.lichkin.framework.defines.exceptions.LKRuntimeException;
 import com.lichkin.framework.web.annotations.LKApiType;
 import com.lichkin.framework.web.enums.ApiType;
 import com.lichkin.springframework.controllers.ApiKeyValues;
 import com.lichkin.springframework.controllers.LKApiYYController;
 import com.lichkin.springframework.services.LKApiService;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 @RestController(Statics.CONTROLLER_NAME)
 @RequestMapping(value = LKFrameworkStatics.WEB_MAPPING_API + Statics.SUB_URL)
 @LKApiType(apiType = ApiType.COMPANY_QUERY)
 public class C extends LKApiYYController<I, Page<O>, Page<O>> {
+
+	@Getter
+	@RequiredArgsConstructor
+	enum ErrorCodes implements LKCodeEnum {
+
+		/** 不是员工 */
+		YOU_ARE_NOT_A_EMPLOYEE(29003),
+
+		;
+
+		private final Integer code;
+
+	}
+
 
 	@Override
 	protected boolean saveLog(I cin, ApiKeyValues<I> params) {
@@ -31,6 +51,14 @@ public class C extends LKApiYYController<I, Page<O>, Page<O>> {
 	@Override
 	protected LKApiService<I, Page<O>> getService(I cin, ApiKeyValues<I> params) {
 		return service;
+	}
+
+
+	@Override
+	protected void beforeInvokeService(I cin, ApiKeyValues<I> params) throws LKException {
+		if (StringUtils.isBlank(cin.getDatas().getToken())) {
+			throw new LKRuntimeException(ErrorCodes.YOU_ARE_NOT_A_EMPLOYEE);
+		}
 	}
 
 
